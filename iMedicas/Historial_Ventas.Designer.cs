e2b9c332a5b6aa614f005459a66ed6b9cbc52aea
Articfire <martinhurtadoarellanolol@hotmail.com>
2017-11-29 15:01:19 -0700

    Hice el Diseño de la forma "Historial_Ventas"

---
+++ iMedicas/Historial_Ventas.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace iMedicas
+{
+    partial class Historial_Ventas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgv_HistorialVentas = new MetroFramework.Controls.MetroGrid();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NombreCte = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Estado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Total = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn_CancelarVenta = new MetroFramework.Controls.MetroButton();
+            this.btn_Detalles = new MetroFramework.Controls.MetroButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_HistorialVentas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv_HistorialVentas
+            // 
+            this.dgv_HistorialVentas.AllowUserToResizeRows = false;
+            this.dgv_HistorialVentas.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dgv_HistorialVentas.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgv_HistorialVentas.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dgv_HistorialVentas.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv_HistorialVentas.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgv_HistorialVentas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_HistorialVentas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.NombreCte,
+            this.Estado,
+            this.Total});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgv_HistorialVentas.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgv_HistorialVentas.EnableHeadersVisualStyles = false;
+            this.dgv_HistorialVentas.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.dgv_HistorialVentas.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dgv_HistorialVentas.Location = new System.Drawing.Point(23, 63);
+            this.dgv_HistorialVentas.Name = "dgv_HistorialVentas";
+            this.dgv_HistorialVentas.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv_HistorialVentas.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgv_HistorialVentas.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dgv_HistorialVentas.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgv_HistorialVentas.Size = new System.Drawing.Size(487, 256);
+            this.dgv_HistorialVentas.TabIndex = 0;
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            this.Id.Width = 110;
+            // 
+            // NombreCte
+            // 
+            this.NombreCte.HeaderText = "Nombre del Cliente";
+            this.NombreCte.Name = "NombreCte";
+            this.NombreCte.Width = 110;
+            // 
+            // Estado
+            // 
+            this.Estado.HeaderText = "Estado de Venta";
+            this.Estado.Name = "Estado";
+            this.Estado.Width = 110;
+            // 
+            // Total
+            // 
+            this.Total.HeaderText = "Total";
+            this.Total.Name = "Total";
+            this.Total.Width = 110;
+            // 
+            // btn_CancelarVenta
+            // 
+            this.btn_CancelarVenta.Location = new System.Drawing.Point(396, 325);
+            this.btn_CancelarVenta.Name = "btn_CancelarVenta";
+            this.btn_CancelarVenta.Size = new System.Drawing.Size(114, 35);
+            this.btn_CancelarVenta.TabIndex = 1;
+            this.btn_CancelarVenta.Text = "Cancelar";
+            this.btn_CancelarVenta.UseSelectable = true;
+            // 
+            // btn_Detalles
+            // 
+            this.btn_Detalles.Location = new System.Drawing.Point(263, 325);
+            this.btn_Detalles.Name = "btn_Detalles";
+            this.btn_Detalles.Size = new System.Drawing.Size(114, 35);
+            this.btn_Detalles.TabIndex = 1;
+            this.btn_Detalles.Text = "Detalles...";
+            this.btn_Detalles.UseSelectable = true;
+            // 
+            // Historial_Ventas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 383);
+            this.Controls.Add(this.btn_Detalles);
+            this.Controls.Add(this.btn_CancelarVenta);
+            this.Controls.Add(this.dgv_HistorialVentas);
+            this.Name = "Historial_Ventas";
+            this.Text = "Historial de ventas";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_HistorialVentas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroGrid dgv_HistorialVentas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NombreCte;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Estado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Total;
+        private MetroFramework.Controls.MetroButton btn_CancelarVenta;
+        private MetroFramework.Controls.MetroButton btn_Detalles;
+    }
+}
